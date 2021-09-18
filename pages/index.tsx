b2744e4fbfff780166a781b1413374c4272f513a
BuildTools <jig5515@outlook.com>
2021-09-17 21:04:45 -0400

    master: initial project setup.

---
+++ pages/index.tsx
@@ -0,0 +1,196 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Home() {
+
+  const test: String = 'hello';
+
+  return (
+    <div className="container">
+      <Head>
+        <title>Cookie Cloner</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">
+          COOKIE CLONER
+        </h1>
+
+        <div className="grid">
+          <Link href="/cookie-cloner/game">
+            <a className="card">
+              <h3>Game &rarr;</h3>
+              <p>Click here to begin your cookie clicking journey!</p>
+            </a>
+          </Link>
+
+          <Link href="/cookie-cloner/about">
+            <a className="card">
+              <h3>About &rarr;</h3>
+              <p>Learn more about this project and its creators!</p>
+            </a>
+          </Link>
+
+          <Link href="/cookie-cloner/recipes">
+            <a className="card">
+              <h3>Recipes &rarr;</h3>
+              <p>Discover new types of cookies and how to make them!</p>
+            </a>
+          </Link>
+
+          <Link href="/cookie-cloner/support-us">
+            <a className="card">
+              <h3>Support Us &rarr;</h3>
+              <p>Learn more about how you can help to support this project!</p>
+            </a>
+          </Link>
+        </div>
+      </main>
+
+      <footer>
+        <span>Powered by{' '}
+          <img src="/favicon.ico" alt="Vercel" className="logo" />
+          s and users like you.
+        </span>
+      </footer>
+
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer img {
+          margin-left: 0.5rem;
+        }
+
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+          text-align: center;
+        }
+
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+
+          max-width: 800px;
+          margin-top: 3rem;
+        }
+
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+
+        .logo {
+          height: 1em;
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
