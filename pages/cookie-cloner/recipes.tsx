fae7fd4c8b1d01b20eb01f826ce22a5ce0c752cd
BuildTools <jig5515@outlook.com>
2021-09-23 11:07:58 -0400

    CC-14: Clean up unused code and imports.

--- pages/cookie-cloner/recipes.tsx
+++ pages/cookie-cloner/recipes.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import Navbar from '../../components/nav/nav';
 import FontAdjust from '../../components/head/head'
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import Link from 'next/link'
 import FontAdjust from '../components/head/head'
 
