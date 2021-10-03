61b67a69db9a58eebde3250c2b3c2b9b6480c997
BuildTools <jig5515@outlook.com>
2021-10-03 02:19:01 -0400

    CC-22: Add classes for all building upgrades.

---
+++ src/app/models/upgrades/buildings/Grandma.ts
@@ -0,0 +1,14 @@
+export class GrandmaUpgrades {
+    id: number = 1;
+    name: string = 'Grandma'
+    icon: string = '../images/grandma.png'
+    level: number = 0;
+    unlockAmount: number[] = [0, 1, 1, 10, 25, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500];
+    buyCost: number[] = 
+        [0, 1000, 5000, 50000, 5000000, 500000000, 50000000000, 50000000000000, 50000000000000000, 50000000000000000000, 
+            50000000000000000000000, 500000000000000000000000000, 5000000000000000000000000000000, 50000000000000000000000000000000000];
+
+    constructor(level: number) {
+        this.level = level;
+    }
+} 
